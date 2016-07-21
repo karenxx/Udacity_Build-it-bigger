bbff12ca68e5ea75512a1026253a57c428fe3de5
XX GUO <xxguo000@gmail.com>
2016-07-20 17:59:50 -0700

    minor changes

--- jokejavalib/src/main/java/com/example/JokeTelling.java
+++ jokejavalib/src/main/java/com/example/JokeTelling.java
@@ -7,11 +7,12 @@ public class JokeTelling {
     private Random random;
 
     public JokeTelling() {
-        jokes = new String[4];
-        jokes[0]= "1";
-        jokes[1] = "2";
-        jokes[2] = "3";
-        jokes[3] = "4";
+        jokes = new String[5];
+        jokes[0] = "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.";
+        jokes[1] = "It is so cold outside I saw a politician with his hands in his own pockets.";
+        jokes[2] = "I wanted to grow my own food but I couldn’t get bacon seeds anywhere.";
+        jokes[3] = "I can’t believe I forgot to go to the gym today. That’s 7 years in a row now.";
+        jokes[4] = "One state official to the other: \"I don't know what people have against us - We haven't done anything.";
 
         random = new Random();
     }
